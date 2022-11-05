6fef16dc2eadd47c2fa634685c731fab72f207a7
jayrugman <jasonrhardman@gmail.com>
2022-11-05 15:16:50 -0600

    developed sleep and clear

--- spead_read.py
+++ spead_read.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+from os import system, name as os_name
+from time import sleep
+
+
+def clear():
+    '''Clears the screen depending on the os name'''
+    if os_name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
 
 the_text = 'material.txt'
 with open(the_text, 'r') as file:
@@ -7,3 +18,15 @@ with open(the_text, 'r') as file:
 
 the_line = the_lines[0].split(' ')  # puts each word as list item in "the_line"
 ' '.join(the_line[0:5])  # prints 5 words of the line, space separated
+
+first_word = 0
+last_word = 1
+sleep_time = 0.2
+while True:
+    if last_word == 10:
+        break
+    print(' '.join(the_line[first_word:last_word]))
+    sleep(sleep_time)
+    clear()
+    first_word += 1
+    last_word += 1
