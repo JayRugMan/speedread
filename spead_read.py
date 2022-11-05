c4831c6b5b4bb9996f1a8f4570668efe0d285eff
jayrugman <jasonrhardman@gmail.com>
2022-11-05 11:37:14 -0600

    corrected the open command

--- spead_read.py
+++ spead_read.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-with open('material.txt', 'r') as file:
-    the_lines = [i for i in file.read().split('\n') in len(i) != 0]
+
+the_text = 'material.txt'
+with open(the_text, 'r') as file:
+    the_lines = [i for i in file.read().split('\n') if len(i) != 0]
 
 the_line = the_lines[0].split(' ')  # puts each word as list item in "the_line"
 ' '.join(the_line[0:5])  # prints 5 words of the line, space separated
